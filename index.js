6c735b0d4b6fa2e7c805c05caf562ac8330a263c
Alexandra <alexandraneff@live.com>
2022-09-11 17:47:16 -0500

    commit tree ab18dde3a48810b3864a3a3918a5bca074d9ee1e

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
-
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i]
+    for (let j = i + 1; j < array.length; j++ ) {
+      if (array[j] === complement) return true
+    }
+  }
+  return false 
+} 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+  can do this similarly to how we did the palindrome one - starting from 
+  index[i] and including index[j] have the first letter add to the last letter
+  then the second letter add to the second to last letter; each time checking
+  to see if the sum equals the target number 
 */
 
 /*
@@ -29,6 +37,7 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  
 }
 
 module.exports = hasTargetSum;
